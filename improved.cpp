226401adbc54db6c875ef9be4cd36ee4d2723d7f
George Patterson <g-patt@outlook.com>
2021-11-04 23:26:19 +1000

    parallelize bacteria loading -> LoadAllBacteriaParallel()

--- improved.cpp
+++ improved.cpp
@@ -5,6 +5,11 @@
 #include <math.h>
 #include <fstream>
 #include <iostream>
+#include <omp.h>
+
+
+#define PARA_LOAD       true
+
 
 int number_bacteria;
 char** bacteria_name;
@@ -17,289 +22,306 @@ short code[27] = { 0, 2, 1, 2, 3, 4, 5, 6, 7, -1, 8, 9, 10, 11, -1, 12, 13, 14, 
 
 void Init()
 {
-	M2 = 1;
-	for (int i=0; i<LEN-2; i++)	// M2 = AA_NUMBER ^ (LEN-2);
-		M2 *= AA_NUMBER; 
-	M1 = M2 * AA_NUMBER;		// M1 = AA_NUMBER ^ (LEN-1);
-	M  = M1 *AA_NUMBER;			// M  = AA_NUMBER ^ (LEN);
+    M2 = 1;
+    for (int i=0; i<LEN-2; i++)	// M2 = AA_NUMBER ^ (LEN-2);
+        M2 *= AA_NUMBER;
+    M1 = M2 * AA_NUMBER;		// M1 = AA_NUMBER ^ (LEN-1);
+    M  = M1 *AA_NUMBER;			// M  = AA_NUMBER ^ (LEN);
 }
 
 class Bacteria
 {
 private:
-	long* vector;
-	long* second;
-	long one_l[AA_NUMBER];
-	long indexs;
-	long total;
-	long total_l;
-	long complement;
+    long* vector;
+    long* second;
+    long one_l[AA_NUMBER];
+    long indexs;
+    long total;
+    long total_l;
+    long complement;
 
-	void InitVectors()
-	{
-		vector = new long [M];
-		second = new long [M1];
-		memset(vector, 0, M * sizeof(long));
-		memset(second, 0, M1 * sizeof(long));
-		memset(one_l, 0, AA_NUMBER * sizeof(long));
-		total = 0;
-		total_l = 0;
-		complement = 0;
-	}
+    void InitVectors()
+    {
+        vector = new long [M];
+        second = new long [M1];
+        memset(vector, 0, M * sizeof(long));
+        memset(second, 0, M1 * sizeof(long));
+        memset(one_l, 0, AA_NUMBER * sizeof(long));
+        total = 0;
+        total_l = 0;
+        complement = 0;
+    }
 
-	void init_buffer(char* buffer)
-	{
-		complement++;
-		indexs = 0;
-		for (int i=0; i<LEN-1; i++)
-		{
-			short enc = encode(buffer[i]);
-			one_l[enc]++;
-			total_l++;
-			indexs = indexs * AA_NUMBER + enc;
-		}
-		second[indexs]++;
-	}
+    void init_buffer(char* buffer)
+    {
+        complement++;
+        indexs = 0;
+        for (int i=0; i<LEN-1; i++)
+        {
+            short enc = encode(buffer[i]);
+            one_l[enc]++;
+            total_l++;
+            indexs = indexs * AA_NUMBER + enc;
+        }
+        second[indexs]++;
+    }
 
-	void cont_buffer(char ch)
-	{
-		short enc = encode(ch);
-		one_l[enc]++;
-		total_l++;
-		long index = indexs * AA_NUMBER + enc;
-		vector[index]++;
-		total++;
-		indexs = (indexs % M2) * AA_NUMBER + enc;
-		second[indexs]++;
-	}
+    void cont_buffer(char ch)
+    {
+        short enc = encode(ch);
+        one_l[enc]++;
+        total_l++;
+        long index = indexs * AA_NUMBER + enc;
+        vector[index]++;
+        total++;
+        indexs = (indexs % M2) * AA_NUMBER + enc;
+        second[indexs]++;
+    }
 
 public:
-	long count;
-	double* tv;
-	long *ti;
+    long count;
+    double* tv;
+    long *ti;
 
-	Bacteria(char* filename)
-	{
+    Bacteria(char* filename)
+    {
 
-		std::ifstream bacteria_file{filename};
+        std::ifstream bacteria_file{filename};
 
-		if (!bacteria_file)
-		{
-			// Print an error and exit
-			std::string fname = filename;
-			std::cerr << "Uh oh, " + fname + " could not be opened for reading!" << std::endl;
-			exit(1);
-		}
+        if (!bacteria_file)
+        {
+            // Print an error and exit
+            std::string fname = filename;
+            std::cerr << "Uh oh, " + fname + " could not be opened for reading!" << std::endl;
+            exit(1);
+        }
 
-		InitVectors();
+        InitVectors();
 
-		char ch;
-		while (bacteria_file.get(ch))
-		{
+        char ch;
+        while (bacteria_file.get(ch))
+        {
 
-			if (ch == '>')
-			{
-				//while (bacteria_file.get(ch) && ch != '\n'); // skip rest of line
-				std::string skip;
-				std::getline(bacteria_file, skip);
+            if (ch == '>')
+            {
+                //while (bacteria_file.get(ch) && ch != '\n'); // skip rest of line
+                std::string skip;
+                std::getline(bacteria_file, skip);
 
-				char buffer[LEN-1];
-				bacteria_file.read(buffer,LEN-1);
-				init_buffer(buffer);
-			}
-			else if (ch != '\n' && ch != '\r')
-				cont_buffer(ch);
-		}
+                char buffer[LEN-1];
+                bacteria_file.read(buffer,LEN-1);
+                init_buffer(buffer);
+            }
+            else if (ch != '\n' && ch != '\r')
+                cont_buffer(ch);
+        }
 
-		long total_plus_complement = total + complement;
-		double total_div_2 = total * 0.5;
-		int i_mod_aa_number = 0;
-		int i_div_aa_number = 0;
-		long i_mod_M1 = 0;
-		long i_div_M1 = 0;
+        long total_plus_complement = total + complement;
+        double total_div_2 = total * 0.5;
+        int i_mod_aa_number = 0;
+        int i_div_aa_number = 0;
+        long i_mod_M1 = 0;
+        long i_div_M1 = 0;
 
-		double one_l_div_total[AA_NUMBER];
-		for (int i=0; i<AA_NUMBER; i++)
-			one_l_div_total[i] = (double)one_l[i] / total_l;
-		
-		double* second_div_total = new double[M1];
-		for (int i=0; i<M1; i++)
-			second_div_total[i] = (double)second[i] / total_plus_complement;
+        double one_l_div_total[AA_NUMBER];
+        for (int i=0; i<AA_NUMBER; i++)
+            one_l_div_total[i] = (double)one_l[i] / total_l;
 
-		count = 0;
-		double* t = new double[M];
+        double* second_div_total = new double[M1];
+        for (int i=0; i<M1; i++)
+            second_div_total[i] = (double)second[i] / total_plus_complement;
 
-		for(long i=0; i<M; i++)
-		{
-			double p1 = second_div_total[i_div_aa_number];
-			double p2 = one_l_div_total[i_mod_aa_number];
-			double p3 = second_div_total[i_mod_M1];
-			double p4 = one_l_div_total[i_div_M1];
-			double stochastic =  (p1 * p2 + p3 * p4) * total_div_2;
+        count = 0;
+        double* t = new double[M];
 
-			if (i_mod_aa_number == AA_NUMBER-1)
-			{
-				i_mod_aa_number = 0;
-				i_div_aa_number++;
-			}
-			else
-				i_mod_aa_number++;
+        for(long i=0; i<M; i++)
+        {
+            double p1 = second_div_total[i_div_aa_number];
+            double p2 = one_l_div_total[i_mod_aa_number];
+            double p3 = second_div_total[i_mod_M1];
+            double p4 = one_l_div_total[i_div_M1];
+            double stochastic =  (p1 * p2 + p3 * p4) * total_div_2;
 
-			if (i_mod_M1 == M1-1)
-			{
-				i_mod_M1 = 0;
-				i_div_M1++;
-			}
-			else
-				i_mod_M1++;
+            if (i_mod_aa_number == AA_NUMBER-1)
+            {
+                i_mod_aa_number = 0;
+                i_div_aa_number++;
+            }
+            else
+                i_mod_aa_number++;
 
-			if (stochastic > EPSILON) 
-			{
-				t[i] = (vector[i] - stochastic) / stochastic;
-				count++;
-			}
-			else
-				t[i] = 0;
-		}
-		
-		delete second_div_total;
-		delete vector;
-		delete second;
+            if (i_mod_M1 == M1-1)
+            {
+                i_mod_M1 = 0;
+                i_div_M1++;
+            }
+            else
+                i_mod_M1++;
 
-		tv = new double[count];
-		ti = new long[count];
+            if (stochastic > EPSILON)
+            {
+                t[i] = (vector[i] - stochastic) / stochastic;
+                count++;
+            }
+            else
+                t[i] = 0;
+        }
 
-		int pos = 0;
-		for (long i=0; i<M; i++)
-		{
-			if (t[i] != 0)
-			{
-				tv[pos] = t[i];
-				ti[pos] = i;
-				pos++;
-			}
-		}
-		delete t;
+        delete second_div_total;
+        delete vector;
+        delete second;
 
-		//fclose (bacteria_file);
-	}
+        tv = new double[count];
+        ti = new long[count];
+
+        int pos = 0;
+        for (long i=0; i<M; i++)
+        {
+            if (t[i] != 0)
+            {
+                tv[pos] = t[i];
+                ti[pos] = i;
+                pos++;
+            }
+        }
+        delete t;
+
+        //fclose (bacteria_file);
+    }
 };
 
 void ReadInputFile(const char* input_name)
 {
 
-	std::ifstream input_file{input_name};
+    std::ifstream input_file{input_name};
 
-	if (!input_file)
-	{
-		std::string iname = input_name;
-		std::cerr << "Error: failed to open file " + iname + " (Hint: check your working directory)\n" << std::endl;
-		exit(1);
-	}
+    if (!input_file)
+    {
+        std::string iname = input_name;
+        std::cerr << "Error: failed to open file " + iname + " (Hint: check your working directory)\n" << std::endl;
+        exit(1);
+    }
 
-	input_file >> number_bacteria;
-	bacteria_name = new char*[number_bacteria];
+    input_file >> number_bacteria;
+    bacteria_name = new char*[number_bacteria];
 
-	for(long i=0;i<number_bacteria;i++)
-	{
-		char name[10];
-		input_file >> name;
-		bacteria_name[i] = new char[20];
-		sprintf(bacteria_name[i], "data/%s.faa", name);
-	}
-	//fclose(input_file);
+    for(long i=0;i<number_bacteria;i++)
+    {
+        char name[10];
+        input_file >> name;
+        bacteria_name[i] = new char[20];
+        sprintf(bacteria_name[i], "data/%s.faa", name);
+    }
+    //fclose(input_file);
 }
 
 double CompareBacteria(Bacteria* b1, Bacteria* b2)
 {
-	double correlation = 0;
-	double vector_len1=0;
-	double vector_len2=0;
-	long p1 = 0;
-	long p2 = 0;
-	while (p1 < b1->count && p2 < b2->count)
-	{
-		long n1 = b1->ti[p1];
-		long n2 = b2->ti[p2];
-		if (n1 < n2)
-		{
-			double t1 = b1->tv[p1];
-			vector_len1 += (t1 * t1);
-			p1++;
-		}
-		else if (n2 < n1)
-		{
-			double t2 = b2->tv[p2];
-			p2++;
-			vector_len2 += (t2 * t2);
-		}
-		else
-		{
-			double t1 = b1->tv[p1++];
-			double t2 = b2->tv[p2++];
-			vector_len1 += (t1 * t1);
-			vector_len2 += (t2 * t2);
-			correlation += t1 * t2;
-		}
-	}
-	while (p1 < b1->count)
-	{
-		long n1 = b1->ti[p1];
-		double t1 = b1->tv[p1++];
-		vector_len1 += (t1 * t1);
-	}
-	while (p2 < b2->count)
-	{
-		long n2 = b2->ti[p2];
-		double t2 = b2->tv[p2++];
-		vector_len2 += (t2 * t2);
-	}
+    double correlation = 0;
+    double vector_len1=0;
+    double vector_len2=0;
+    long p1 = 0;
+    long p2 = 0;
+    while (p1 < b1->count && p2 < b2->count)
+    {
+        long n1 = b1->ti[p1];
+        long n2 = b2->ti[p2];
+        if (n1 < n2)
+        {
+            double t1 = b1->tv[p1];
+            vector_len1 += (t1 * t1);
+            p1++;
+        }
+        else if (n2 < n1)
+        {
+            double t2 = b2->tv[p2];
+            p2++;
+            vector_len2 += (t2 * t2);
+        }
+        else
+        {
+            double t1 = b1->tv[p1++];
+            double t2 = b2->tv[p2++];
+            vector_len1 += (t1 * t1);
+            vector_len2 += (t2 * t2);
+            correlation += t1 * t2;
+        }
+    }
+    while (p1 < b1->count)
+    {
+        long n1 = b1->ti[p1];
+        double t1 = b1->tv[p1++];
+        vector_len1 += (t1 * t1);
+    }
+    while (p2 < b2->count)
+    {
+        long n2 = b2->ti[p2];
+        double t2 = b2->tv[p2++];
+        vector_len2 += (t2 * t2);
+    }
 
-	return correlation / (sqrt(vector_len1) * sqrt(vector_len2));
+    return correlation / (sqrt(vector_len1) * sqrt(vector_len2));
 }
 
 Bacteria** LoadAllBacteria() {
-	Bacteria** b = new Bacteria*[number_bacteria];
-	for(int i=0; i<number_bacteria; i++) {
-		printf("load %d of %d\n", i+1, number_bacteria);
-		b[i] = new Bacteria(bacteria_name[i]);
-	}
-	return b;
+    Bacteria** b = new Bacteria*[number_bacteria];
+
+    for(int i=0; i<number_bacteria; i++) {
+        printf("[Thread: %d] load %d of %d\n", 
+            omp_get_thread_num(), i+1, number_bacteria);
+        b[i] = new Bacteria(bacteria_name[i]);
+    }
+
+    return b;
+}
+
+Bacteria** LoadAllBacteriaParallel() {
+    Bacteria** b = new Bacteria*[number_bacteria];
+
+    #pragma omp parallel
+    #pragma omp for
+    for(int i=0; i<number_bacteria; i++) {
+        printf("[Thread: %d] load %d of %d\n", 
+            omp_get_thread_num(), i+1, number_bacteria);
+        b[i] = new Bacteria(bacteria_name[i]);
+    }
+
+    return b;
 }
 
 void CompareAllBacteria(Bacteria** b)
 {
     for(int i=0; i<number_bacteria-1; i++) {
-		for(int j=i+1; j<number_bacteria; j++) {
-			printf("%2d %2d -> ", i, j);
-			double correlation = CompareBacteria(b[i], b[j]);
-			printf("%.20lf\n", correlation);
-		}
-	}
+        for(int j=i+1; j<number_bacteria; j++) {
+            printf("%2d %2d -> ", i, j);
+            double correlation = CompareBacteria(b[i], b[j]);
+            printf("%.20lf\n", correlation);
+        }
+    }
 }
 
 int main(int argc,char * argv[])
 {
-	time_t st = time(NULL);
+    time_t st = time(NULL);
+
+    time_t t1 = st;
+    Init();
+    ReadInputFile("list.txt");
+    time_t t2 = time(NULL);
+    printf("Init & ReadInputFile: %d seconds\n",  t2 -t1);
+
+    t1 = t2;
+    Bacteria** b;
+    if (PARA_LOAD) { b = LoadAllBacteriaParallel(); } else { LoadAllBacteria(); }
+    t2 = time(NULL);
+    printf("\nLoadAllBacteria: %d seconds\n",  t2 -t1);
+
+    t1 = t2;
+    CompareAllBacteria(b);
+    t2 = time(NULL);
+    printf("CompareAllBacteria: %d seconds\n",  t2 -t1);
 
 
-	time_t t1 = st;
-	Init();
-	ReadInputFile("list.txt");
-	time_t t2 = time(NULL);
-	printf("Init & ReadInputFile: %d seconds\n",  t2 -t1);
-
-	t1 = t2;
-	Bacteria** b = LoadAllBacteria();
-	t2 = time(NULL);
-	printf("LoadAllBacteria: %d seconds\n",  t2 -t1);
-
-	t1 = t2;
-	CompareAllBacteria(b);
-	t2 = time(NULL);
-	printf("CompareAllBacteria: %d seconds\n",  t2 -t1);
-	
-	
-	printf("Total time elapsed: %d seconds\n",  t2 -st);
-	return 0;
+    printf("Total time elapsed: %d seconds\n",  t2 -st);
+    return 0;
 }
